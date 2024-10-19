f230dc4638c4a416b3b5129429cfb18841a5eb31
OnGod123 <ifejikav4@gmail.com>
2024-10-19 04:47:22 +0100

    a'deployment_stage

--- Cloud_Hoskie/settings.py
+++ Cloud_Hoskie/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-fddj0p!o507bt&ks!j3if344p5snrbi=n4*r9_)8)-=y$i8la(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['hoskie.onrender.com']
+
 
 
 # Application definition
@@ -236,7 +237,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'myapp/staticfiles/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'myapp/media/') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
