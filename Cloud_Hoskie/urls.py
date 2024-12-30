380b64157d8815134442fe895b7ee6a8fd25bb92
OnGod123 <ifejikav4@gmail.com>
2024-12-30 17:35:42 +0000

    still pushing to prod

--- Cloud_Hoskie/urls.py
+++ Cloud_Hoskie/urls.py
@@ -1,21 +1,7 @@
-"""
-URL configuration for Cloud_Hoskie project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,16 +11,17 @@ urlpatterns = [
     path('logout/', include('myapp.logout.urls')),
     path('video-call/', include('myapp.video_call.urls')),
     path('video/', include('myapp.profile.video.urls')),
-    path('chat/', include('myapp.chat.urls'),
-    path('Tweet/', include('myapp.Tweet.urls'),
-    path('Voice_messaging/', Include('myapp.Voice_message.urls'),
+    path('chat/', include('myapp.chat.urls')),
+    path('Tweet/', include('myapp.Tweet.urls')),  # Fixed missing parenthesis
+    path('Voice_messaging/', include('myapp.Voice_message.urls')),  # Fixed capitalization and missing parenthesis
     path('live_app/', include('myapp.live_go.urls')),
-    path(" ", imclude('myapp.file_upload.urls')),
+    path(" ", include('myapp.file_upload.urls')),  # Fixed capitalization of 'imclude'
     path(" ", include('myapp.wallet.urls')),
-    path(" ", include('myapp.authenticate.urls'),
-    path('image/', include('myapp.profile.Image.urls'))
+    path(" ", include('myapp.authenticate.urls')),  # Fixed missing parenthesis
+    path('image/', include('myapp.profile.Image.urls')),
 ]
 
 if settings.DEBUG:
-     urlpatterns += static(settings.VIDEO_URL, document_root=settings.VIDEO_ROOT)
-     urlpatterns += static(settings.IMAGE_URL, document_root=settings.IMAGE_ROOT)
+    urlpatterns += static(settings.VIDEO_URL, document_root=settings.VIDEO_ROOT)
+    urlpatterns += static(settings.IMAGE_URL, document_root=settings.IMAGE_ROOT)
+
