fb2e841e353d4b92921fd9b1c9d9534d66d0f9f6
Vincent <youremail@example.com>
2025-04-24 12:46:50 +0100

    stil working on the url

--- myapp/profile/tweet/trend_views/urls.py
+++ myapp/profile/tweet/trend_views/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import TrendView
 
 urlpatterns = [
-    path('trends/', TrendView.as_view(), name='get_trends'),
+    path('', TrendView.as_view(), name='get_trends'),
     path('trends/<str:trend>/', TrendView.as_view(), name='get_trend_tweets'),
 ]
