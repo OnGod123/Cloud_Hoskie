3d8597daf1e51c5e87f855be26ebaf9f1f3d44d7
Vincent <youremail@example.com>
2025-04-29 09:39:58 +0100

    repointing the redis server client

--- myapp/profile/tweet/trend_views/views.py
+++ myapp/profile/tweet/trend_views/views.py
@@ -7,7 +7,7 @@ import logging
 from django.db import DatabaseError
 
 # Redis setup
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_client = redis.StrictRedis("default")
 
 # Setup logging
 logger = logging.getLogger(__name__)
