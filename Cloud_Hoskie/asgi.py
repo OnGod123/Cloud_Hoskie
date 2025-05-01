b63612fb14257b53643aba1716cfb219c4fa29dc
Vincent <youremail@example.com>
2025-05-01 08:12:23 +0100

    refactoring building and learning

--- Cloud_Hoskie/asgi.py
+++ Cloud_Hoskie/asgi.py
@@ -1,16 +1,15 @@
-"""
-ASGI config for Cloud_Hoskie project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from myapp.profile.video.video_streaming.routing import websocket_urlpatterns  # Correct import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cloud_Hoskie.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
+
--- Cloud_Hoskie/wsgi.py
+++ Cloud_Hoskie/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for Cloud_Hoskie project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from myapp.profile.video.video_streaming.routing import websocket_urlpatterns
 import os
 
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cloud_Hoskie.settings')
-
-application = get_wsgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
